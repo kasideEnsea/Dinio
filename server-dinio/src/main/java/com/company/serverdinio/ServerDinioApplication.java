6f871fc138d92538367a7c43893cc646b0783fe4
Anna <tutivanna2@gmail.com>
2021-05-06 12:19:11 +0300

    add new version of ws server example

---
+++ server-dinio/src/main/java/com/company/serverdinio/ServerDinioApplication.java
@@ -0,0 +1,13 @@
+package com.company.serverdinio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerDinioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerDinioApplication.class, args);
+    }
+
+}
---
+++ server-dinio/src/main/java/com/company/serverdinio/model/User.java
@@ -0,0 +1,5 @@
+package com.company.serverdinio.model;
+
+public class User {
+
+}
